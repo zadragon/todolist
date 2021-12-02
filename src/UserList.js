4443d237d1ae820ff2ba03e68b3f64d290b2b24a
zayong <zayong@feelway.com>
2021-12-02 18:57:28 +0900

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function User({user}){
+    return (
+        <div>
+            <b>{user.username}</b> <span>({user.email})</span>
+        </div>
+    );
+}
+
+
+function UserList({users}){
+    return(
+        <div>
+            {users.map(user => {
+                return <User user={user} key={user.id} />
+            })}
+        </div>
+
+    )
+};
+
+export default UserList;
